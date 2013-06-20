7c596623575b33f4f6adb2c346134d1323219974
pavlov vasil <vpavlov79@yandex.ru>
2013-06-20 16:21:43 +0400

    110 uA

---
+++ encenter/Debug/main.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/cc430x513x.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+
+../main.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/cc430x513x.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
